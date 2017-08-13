2f3c1ba01d92d796298702d6bd54b840ef793c63
Roland Jansen <jansen@stereokrauts.com>
2017-08-13 06:05:00 +0200

    Basic project structure

---
+++ NexusDownloader/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NexusDownloader
+{
+    /// <summary>
+    /// The entry class of the application.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(string arg in args)
+            {
+                string[] argParts = arg.Split('=');
+                string argFlag = argParts[0].ToLower();
+                string argValue = argParts[1].ToLower();
+                Console.WriteLine("flag: {0}, value: {1}", argFlag, argValue);
+            }
+        }
+    }
+}
